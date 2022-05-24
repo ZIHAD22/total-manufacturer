ae058a369fd931608924f99b06ae7537e28c0468
ZIHAD22 <mdzihad2244@gmail.com>
2022-05-24 15:34:05 +0600

    project setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import NavBar from '../Pages/Shared/NavBar'
+import './App.css'
+
+function App() {
+  return (
+    <div className="max-w-[1400px] mx-auto">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
