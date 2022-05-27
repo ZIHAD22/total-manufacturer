be6bc060a1331d749122e105d3d5a85c5ca5d2f8
ZIHAD22 <mdzihad2244@gmail.com>
2022-05-28 00:19:18 +0600

    add product by admin on database

--- src/Pages/Dashboard/AddProduct.jsx
+++ src/Pages/Dashboard/AddProduct.jsx
@@ -1,9 +1,140 @@
+import axios from "../../utility/axios";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const handleAddProduct = async (data) => {
+    const { toolName, toolDescription, minOrder, quantity, price, postedImg } =
+      data;
+    const image = postedImg[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMAGE_STORAGE_KEY}`;
+    const { data: imgPostData } = await axios.post(url, formData);
+    const storedImgUrl = imgPostData.data.url;
+    const product = {
+      toolName,
+      toolDescription,
+      minOrder,
+      quantity,
+      price,
+      toolImg: storedImgUrl,
+    };
+    const { data: postedProduct } = await axios.post("products", product);
+    console.log(postedProduct);
+  };
   return (
     <div>
-      <h1>this is add products page</h1>
+      <h1 className="text-4xl text-center font-bold">
+        Dashboard (Add Product)
+      </h1>
+
+      <div className="">
+        <form onSubmit={handleSubmit(handleAddProduct)}>
+          <h4 className="text-xl text-center mt-5">Add Product</h4>
+          <div class="form-control w-full max-w-lg mx-auto">
+            <label class="label">
+              <span class="label-text">Product Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              {...register("toolName", { required: "required" })}
+              class="input input-bordered w-full max-w-lg"
+            />
+            {errors.toolName?.message && (
+              <p className="text-red-600">{errors.toolName?.message}</p>
+            )}
+          </div>
+          <div class="form-control w-full max-w-lg mx-auto">
+            <label class="label">
+              <span class="label-text">Product Description</span>
+            </label>
+            <textarea
+              type="text"
+              placeholder="Type here"
+              {...register("toolDescription", { required: "required" })}
+              class="textarea  input-bordered w-full max-w-lg"
+            />
+            {errors.toolDescription?.message && (
+              <p className="text-red-600">{errors.toolDescription?.message}</p>
+            )}
+          </div>
+          <div class="form-control w-full max-w-lg mx-auto">
+            <label class="label">
+              <span class="label-text">Minimum Order Quantity</span>
+            </label>
+            <input
+              type="number"
+              placeholder="Type here"
+              {...register("minOrder", { required: "required" })}
+              class="input input-bordered w-full max-w-lg"
+            />
+            {errors.minOrder?.message && (
+              <p className="text-red-600">{errors.minOrder?.message}</p>
+            )}
+          </div>
+          <div class="form-control w-full max-w-lg mx-auto">
+            <label class="label">
+              <span class="label-text">Available Quantity</span>
+            </label>
+            <input
+              type="number"
+              placeholder="Type here"
+              {...register("quantity", { required: "required" })}
+              class="input input-bordered w-full max-w-lg"
+            />
+            {errors.quantity?.message && (
+              <p className="text-red-600">{errors.quantity?.message}</p>
+            )}
+          </div>
+          <div class="form-control w-full max-w-lg mx-auto">
+            <label class="label">
+              <span class="label-text">
+                Product Price <small>(per unit)</small>
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type here"
+              {...register("price", { required: "required" })}
+              class="input input-bordered w-full max-w-lg"
+            />
+            {errors.price?.message && (
+              <p className="text-red-600">{errors.price?.message}</p>
+            )}
+          </div>
+
+          <div class="form-control w-full max-w-lg mx-auto">
+            <label class="label">
+              <span class="label-text">Product Image</span>
+            </label>
+            <input
+              type="file"
+              {...register("postedImg", { required: "required" })}
+              class="input input-bordered w-full max-w-lg"
+            />
+            {errors.postedImg?.message && (
+              <p className="text-red-600">{errors.postedImg?.message}</p>
+            )}
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="btn btn-success w-1/2 mx-auto block my-2 text-white font-bold"
+            >
+              Add Product
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
