b9e30d86b31797b85da055ea4247a11a0b477a71
ZIHAD22 <mdzihad2244@gmail.com>
2022-05-28 17:29:57 +0600

    add some change

--- src/Pages/Home/Tools.jsx
+++ src/Pages/Home/Tools.jsx
@@ -17,7 +17,7 @@ const Tools = () => {
     <div className="mt-20">
       <h1 className="text-4xl text-center">Our Products</h1>
       <div className="grid grid-cols-1 gap-y-10  lg:grid-cols-3">
-        {tools.map((tool) => (
+        {tools?.map((tool) => (
           <ToolsItem key={tool._id} tool={tool} />
         ))}
       </div>
